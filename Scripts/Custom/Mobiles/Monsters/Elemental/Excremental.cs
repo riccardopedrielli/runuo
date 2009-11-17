3255f49e9207667fe9ace2dabe728c3f0cbf6d7e
Marco Baccarani <bacca87@gmail.com>
2009-11-17 20:20:32 +0000

    aggiunto l'excremental

---
+++ Scripts/Custom/Mobiles/Monsters/Elemental/Excremental.cs
@@ -0,0 +1,155 @@
+using System;
+using Server;
+using Server.Items;
+
+namespace Server.Mobiles
+{
+	[CorpseName( "an excremental corpse" )]
+	public class Excremental : BaseCreature
+	{
+		[Constructable]
+		public Excremental() : this( 2 )
+		{
+		}
+
+		[Constructable]
+		public Excremental( int oreAmount ) : base( AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4 )
+		{
+			// TODO: Gas attack
+			Name = "a excremental";
+			Body = 112;
+			BaseSoundID = 268;
+            Hue = 442;
+
+			SetStr( 226, 255 );
+			SetDex( 126, 145 );
+			SetInt( 71, 92 );
+
+			SetHits( 1136, 1153 );
+
+			SetDamage( 28 );
+
+			SetDamageType( ResistanceType.Physical, 55 );
+			SetDamageType( ResistanceType.Fire, 25 );
+			SetDamageType( ResistanceType.Cold, 25 );
+			SetDamageType( ResistanceType.Energy, 25 );
+
+			SetResistance( ResistanceType.Physical, 85, 95 );
+			SetResistance( ResistanceType.Fire, 20, 30 );
+			SetResistance( ResistanceType.Cold, 50, 60 );
+			SetResistance( ResistanceType.Poison, 95, 100 );
+			SetResistance( ResistanceType.Energy, 70, 80 );
+
+			SetSkill( SkillName.MagicResist, 80.1, 95.0 );
+			SetSkill( SkillName.Tactics, 90.1, 100.0 );
+			SetSkill( SkillName.Wrestling, 90.1, 100.0 );
+
+			Fame = 3500;
+			Karma = -3500;
+
+			VirtualArmor = 68;
+		}
+
+		public override void GenerateLoot()
+		{
+			AddLoot( LootPack.FilthyRich );
+			AddLoot( LootPack.Gems, 4 );
+		}
+
+		/*** MOD_START ***/
+		/*
+		public override bool AutoDispel{ get{ return true; } }
+		*/
+		public override bool AutoDispel { get { return false; } }
+		/*** MOD_END ***/
+		public override bool BleedImmune{ get{ return true; } }
+		public override int TreasureMapLevel{ get{ return 2; } }
+
+		public override void AlterMeleeDamageFrom( Mobile from, ref int damage )
+		{
+			if ( from is BaseCreature )
+			{
+				BaseCreature bc = (BaseCreature)from;
+
+				if ( bc.Controlled || bc.BardTarget == this )
+					damage = 0; // Immune to pets and provoked creatures
+			}
+		}
+
+		/*** DEL_START ***/
+		//se no troppo sgravo
+		/*
+		public override void CheckReflect( Mobile caster, ref bool reflect )
+		{
+			reflect = true; // Every spell is reflected back to the caster
+		}
+		*/
+		/*** DEL_END ***/
+
+		private DateTime m_NextBomb;
+		private int m_Thrown;
+
+		public override void OnActionCombat()
+		{
+			Mobile combatant = Combatant;
+
+			if ( combatant == null || combatant.Deleted || combatant.Map != Map || !InRange( combatant, 12 ) || !CanBeHarmful( combatant ) || !InLOS( combatant ) )
+				return;
+
+			if ( DateTime.Now >= m_NextBomb )
+			{
+				ThrowBomb( combatant );
+
+				m_Thrown++;
+
+				if ( 0.75 >= Utility.RandomDouble() && (m_Thrown % 2) == 1 ) // 75% chance to quickly throw another boulder
+					m_NextBomb = DateTime.Now + TimeSpan.FromSeconds( 3.0 );
+				else
+					m_NextBomb = DateTime.Now + TimeSpan.FromSeconds( 5.0 + (10.0 * Utility.RandomDouble()) ); // 5-15 seconds
+			}
+		}
+
+		public void ThrowBomb( Mobile m )
+		{
+			DoHarmful( m );
+
+			this.MovingParticles( m, 3899, 2, 0, false, true, 46, 0, 9502, 6014, 46, EffectLayer.Waist, 0 );
+
+			new InternalTimer( m, this ).Start();
+		}
+
+		private class InternalTimer : Timer
+		{
+			private Mobile m_Mobile, m_From;
+
+			public InternalTimer( Mobile m, Mobile from ) : base( TimeSpan.FromSeconds( 1.0 ) )
+			{
+				m_Mobile = m;
+				m_From = from;
+				Priority = TimerPriority.TwoFiftyMS;
+			}
+
+			protected override void OnTick()
+			{
+				m_Mobile.PlaySound( 0x4C9 );
+				AOS.Damage( m_Mobile, m_From, Utility.RandomMinMax( 10, 20 ), 0, 100, 0, 0, 0 );
+			}
+		}
+
+		public Excremental( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+			writer.Write( (int) 0 );
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+			int version = reader.ReadInt();
+		}
+	}
+}
